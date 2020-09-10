9ebde0dbe6404afe1f952fe5cee65dc6cf188298
the-one-who-knoccks <thiagoalves.devp@gmail.com>
2020-09-10 13:35:21 -0300

    :sparkles: Initial commit

--- src/Components/Form/Form.jsx
+++ src/Components/Form/Form.jsx
@@ -12,7 +12,7 @@ const Form = ({ inputText, setInputText, todos, setTodos }) => {
       { text: inputText, completed: false, id: Math.random() + 10000 }
     ]);
 
-  }
+  }  
 
   return (
     <form>
